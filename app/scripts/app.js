6aa65b29ac51635f503ca4447b8626a63a5f77e2
Gaurav Shukla <gshukla66@gmail.com>
2015-10-20 12:20:06 +0530

    Added main and get involved page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,13 +21,33 @@ angular
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        // controller: 'MainCtrl',
+        // controllerAs: 'main',
+        // title: "Concerted|Home"
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+      .when('/docs', {
+        templateUrl: 'views/docs.html',
+        // controller: 'DocsCtrl',
+        // controllerAs: 'docs',
+        // title: "Concerted|Documentation"
+      })
+      .when('/releases', {
+        templateUrl: 'views/releases.html',
+        // controller: 'ReleasesCtrl',
+        // controllerAs: 'releases',
+        // title: "Concerted|Releases"
+      })
+      .when('/news', {
+        templateUrl: 'views/news.html',
+        // controller: 'NewsCtrl',
+        // controllerAs: 'news',
+        // title: "Concerted|News"
+      })
+      .when('/contribute', {
+        templateUrl: 'views/contribute.html',
+        // controller: 'ContributeCtrl',
+        // controllerAs: 'contribute',
+        // title: "Concerted|Get Involved"
       })
       .otherwise({
         redirectTo: '/'
