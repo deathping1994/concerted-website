d68f4703e77c63ef8d7901d025d8da13de0657b9
Gaurav Shukla <gshukla66@gmail.com>
2015-10-23 04:05:22 +0530

    ttest

--- setup-hooks/pre-commit.js
+++ setup-hooks/pre-commit.js
@@ -1,17 +1,37 @@
 var fs = require("fs");
 
-fs.readdir("app/news",function(err, files){
+Object.deepExtend = function(destination, source) {
+  for (var property in source) {
+    if (typeof source[property] === "object" &&
+     source[property] !== null ) {
+      destination[property] = destination[property] || {};
+      arguments.callee(destination[property], source[property]);
+    } else {
+      destination[property] = source[property];
+    }
+  }
+  return destination;
+};
+
+fs.readdir("test-md",function(err, files){
    if (err) {
        return console.error(err);
    }
+       var fileobj={};
        var filelist=[];
    files.forEach( function (file){
-     if((/\.(md)$/i).test(file))
-    {filelist.push("news/"+file);}
+   	console.log(file)
+    if((/\.(md)$/i).test(file)){
+    var res=file.split('-');
+      fileobj.year=res[0];
+      fileobj.month=res[1];
+      fileobj.date=res[2];
+      fileobj.title=res[3].replace('_',' ');
+      var temp={};
+    filelist.push(Object.deepExtend(temp,fileobj));
+   }});
+   fs.writeFile('test-md/newslist.json',filelist,function(err){
+   	console.log(err);
    });
-   fs.writeFile('app/news/newslist.json',JSON.stringify(filelist),function(err){
-   	if(err)
-      return console.log(err);
-   });
-       console.log(filelist);
+       console.log( filelist);
 });
