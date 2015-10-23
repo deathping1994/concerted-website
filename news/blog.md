0425ad68ac9722f6d2f8345d2d8eb45a3ce0c20e
Gaurav Shukla <deathping1994@gmail.com>
2015-10-23 07:14:06 -0400

    tweaks

--- news/blog.md
+++
@@ -1,8 +0,0 @@
-# How to use angular-md
-This is a example of how to use angular-md.
-
-- Bullet point 1.
-- Bullet point 2.
-
-> To use or not to use...
-> - Anonymous
--- setup-hooks/pre-commit.js
+++ setup-hooks/pre-commit.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-fs.readdir("app/news",function(err, files){
+fs.readdir("news",function(err, files){
    if (err) {
        return console.error(err);
    }
@@ -9,9 +9,56 @@ fs.readdir("app/news",function(err, files){
      if((/\.(md)$/i).test(file))
     {filelist.push("news/"+file);}
    });
-   fs.writeFile('app/news/newslist.json',JSON.stringify(filelist),function(err){
+   fs.writeFile('news/newslist.json',JSON.stringify(filelist),function(err){
    	if(err)
       return console.log(err);
    });
        console.log(filelist);
+});
+var fs = require("fs");
+var _=require("lodash");
+
+Object.deepExtend = function(destination, source) {
+  for (var property in source) {
+    if (typeof source[property] === "object" &&
+     source[property] !== null ) {
+      destination[property] = destination[property] || {};
+      arguments.callee(destination[property], source[property]);
+    } else {
+      destination[property] = source[property];
+    }
+  }
+  return destination;
+};
+
+
+
+fs.readdir("news",function(err, files){
+   if (err) {
+       return console.error(err);
+   }
+       var fileobj={};
+       var filelist=[];
+   files.forEach( function (file){
+   	console.log(file)
+    if((/\.(md)$/i).test(file)){
+      fileobj.url="news/"+file;
+      var res=file.split('-');
+      fileobj.year=res[0];
+      fileobj.month=res[1];
+      fileobj.date=res[2];
+      fileobj.title=((res[3].replace(/_/g,' ')).split("."))[0];
+      var temp={};
+    filelist.push(Object.deepExtend(temp,fileobj));
+   }});
+	
+   fs.writeFile('news/newslist.json',JSON.stringify(filelist),function(err){
+   	console.log(err);
+   });
+var archive=_.groupBy(filelist, function(n) {
+  return n.year;
+});
+fs.writeFile('news/archive.json',JSON.stringify(archive),function(err){
+   	console.log(err);
+   });
 });
