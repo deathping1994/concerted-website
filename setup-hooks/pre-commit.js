6a866f810bfc05c7a7d71c5a224eab66ca1014e1
Gaurav Shukla <gshukla66@gmail.com>
2015-10-24 08:49:00 +0530

    testing

--- setup-hooks/pre-commit.js
+++ setup-hooks/pre-commit.js
@@ -13,52 +13,53 @@ Object.deepExtend = function(destination, source) {
   }
   return destination;
 };
+
+
 var folders=[{url:"news"},{url:"markdown-pages"}];
+
 for(folder in folders){
-  console.log(folder);
+  console.log("Searching "+folders[folder].url+"...");
   var url="app/"+folders[folder].url;
-  fs.readdir(url,function(err, files){
-   if (err) {
-       return console.error(err);
-   }
-       var fileobj={};
-       var filelist=[];
-   files.forEach(function (file){
-    console.log(file)
-    if((/\.(md)$/i).test(file)){
-      if (folders[folder].url=="news" || folders[folder].url=="blog"){
-        fileobj.url=folders[folder].url+"/"+file;
-        var res=file.split('-');
-        fileobj.year=res[0];
-        fileobj.month=res[1];
-        fileobj.date=res[2];
-        fileobj.title=((res[3].replace(/_/g,' ')).split("."))[0];
-      }
+  files=fs.readdirSync(url);
+    var fileobj={};
+    var filelist=[];
+    files.forEach(function (file){
+      if((/\.(md)$/i).test(file)){
+        console.log("Indexing "+file);
+        if (folders[folder].url=="news" || folders[folder].url=="blog"){
+          fileobj.url=folders[folder].url+"/"+file;
+          var res=file.split('-');
+          fileobj.year=res[0];
+          fileobj.month=res[1];
+          fileobj.date=res[2];
+          fileobj.title=((res[3].replace(/_/g,' ')).split("."))[0];
+        }
       else{
-        fileobj.url=folders[folder].url+"/"+file;
-        fileobj.title=file.split(".")[0];
-      }
+          fileobj.url=folders[folder].url+"/"+file;
+          fileobj.title=file.split(".")[0];
+        }
       var temp={};
-    filelist.push(Object.deepExtend(temp,fileobj));
-   }
-  });
-  
-   fs.writeFile("app/"+folders[folder].url+"/list.json",JSON.stringify(filelist),function(err){
-    console.log(err);
-   });
-  var archive=_.groupBy(filelist, function(n) {
-  return n.year;
-  });
-  filelist={};
-  if(folders[folder].url=="news" || folders[folder].url=="blog")
-     { fs.writeFile("app/"+folders[folder].url+"/archive.json",JSON.stringify(archive),function(err){
-      if(err)
-        console.log(err);
-      
-
-     });
+      filelist.push(Object.deepExtend(temp,fileobj));
+      }
+    });
+    console.log("Index for "+folders[folder].url+"\n"+filelist);
+    fs.writeFile("app/"+folders[folder].url+"/list.json",JSON.stringify(filelist),function(err){
+      if(err){
+        console.log("Error writing index for "+folders[folder].url+"\n"+err);
+      }
+    });
+    if(folders[folder].url=="news"||folders[folder].url=="blog"){
+      var archive=_.groupBy(filelist, function(n) {
+      return n.year;
+      });
+      console.log("Archive for "+folders[folder].url+"\n"+archive);
+      fs.writeFile("app/"+folders[folder].url+"/archive.json",JSON.stringify(archive),function(err){
+        if(err)
+          console.log(err);
+      });
     }
-   });
-
+    console.log(filelist);
+    filelist=[];
+    console.log(filelist);
 };
 
