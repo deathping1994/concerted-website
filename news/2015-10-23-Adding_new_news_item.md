b6e9888dac80646a235aea8e852909d13539e81d
Gaurav Shukla <gshukla66@gmail.com>
2015-10-24 11:12:17 +0530

    clean up

--- news/2015-10-23-Adding_new_news_item.md
+++
@@ -1,7 +0,0 @@
-#Add new news item
-
->Create a new markdown file
->Name it in **`YEAR-MONTH-DATE-Title_Seperated_by_underscore.md`** 
-		eg:"2015-01-22-Adding_new_news_item.md"
->store it in news folder
->Commit and enjoy !
--- setup-hooks/pre-commit.js
+++ setup-hooks/pre-commit.js
@@ -1,21 +1,4 @@
 var fs = require("fs");
-
-fs.readdir("news",function(err, files){
-   if (err) {
-       return console.error(err);
-   }
-       var filelist=[];
-   files.forEach( function (file){
-     if((/\.(md)$/i).test(file))
-    {filelist.push("news/"+file);}
-   });
-   fs.writeFile('news/newslist.json',JSON.stringify(filelist),function(err){
-   	if(err)
-      return console.log(err);
-   });
-       console.log(filelist);
-});
-var fs = require("fs");
 var _=require("lodash");
 
 Object.deepExtend = function(destination, source) {
@@ -31,34 +14,66 @@ Object.deepExtend = function(destination, source) {
   return destination;
 };
 
+path = require('path').resolve(process.cwd(), 'app');
+function getDirectories(srcpath) {
+  return fs.readdirSync(srcpath).filter(function(file) {
+    return fs.statSync((path+"/"+file)).isDirectory();
+  });
+}
 
-
-fs.readdir("news",function(err, files){
-   if (err) {
-       return console.error(err);
-   }
-       var fileobj={};
-       var filelist=[];
-   files.forEach( function (file){
-   	console.log(file)
-    if((/\.(md)$/i).test(file)){
-      fileobj.url="news/"+file;
-      var res=file.split('-');
-      fileobj.year=res[0];
-      fileobj.month=res[1];
-      fileobj.date=res[2];
-      fileobj.title=((res[3].replace(/_/g,' ')).split("."))[0];
+var folders=[];
+dirs=getDirectories(path);
+for(dir in dirs){
+var folderobj={};
+if(dirs[dir][0]!='.'){
+  folderobj.url=dirs[dir];
+    folders.push(folderobj);
+  }
+}
+console.log(folders);
+for(folder in folders){
+  console.log("Searching "+folders[folder].url+"...");
+  var url="app/"+folders[folder].url;
+  files=fs.readdirSync(url);
+    var fileobj={};
+    var filelist=[];
+    files.forEach(function (file){
+      if((/\.(md)$/i).test(file)){
+        console.log("Indexing "+file);
+        if (folders[folder].url=="news" || folders[folder].url=="blog"){
+          fileobj.url=folders[folder].url+"/"+file;
+          var res=file.split('-');
+          fileobj.year=res[0];
+          fileobj.month=res[1];
+          fileobj.date=res[2];
+          fileobj.title=((res[3].replace(/_/g,' ')).split("."))[0];
+        }
+      else{
+          fileobj.url=folders[folder].url+"/"+file;
+          fileobj.title=file.split(".")[0];
+        }
       var temp={};
-    filelist.push(Object.deepExtend(temp,fileobj));
-   }});
-	
-   fs.writeFile('news/newslist.json',JSON.stringify(filelist),function(err){
-   	console.log(err);
-   });
-var archive=_.groupBy(filelist, function(n) {
-  return n.year;
-});
-fs.writeFile('news/archive.json',JSON.stringify(archive),function(err){
-   	console.log(err);
-   });
-});
+      filelist.push(Object.deepExtend(temp,fileobj));
+      }
+    });
+    console.log("Index for "+folders[folder].url+"\n"+filelist);
+    fs.writeFile("app/"+folders[folder].url+"/list.json",JSON.stringify(filelist),function(err){
+      if(err){
+        console.log("Error writing index for "+folders[folder].url+"\n"+err);
+      }
+    });
+    if(folders[folder].url=="news"||folders[folder].url=="blog"){
+      var archive=_.groupBy(filelist, function(n) {
+      return n.year;
+      });
+      console.log("Archive for "+folders[folder].url+"\n"+archive);
+      fs.writeFile("app/"+folders[folder].url+"/archive.json",JSON.stringify(archive),function(err){
+        if(err)
+          console.log(err);
+      });
+    }
+    console.log(filelist);
+    filelist=[];
+    console.log(filelist);
+};
+
